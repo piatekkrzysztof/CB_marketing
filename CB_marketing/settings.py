6927243f30d026aa21be5e4a459f5a8ad4b6800b
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2023-07-11 13:43:17 +0200

    first app and settings

--- CB_marketing/settings.py
+++ CB_marketing/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
+
 ]
 
 MIDDLEWARE = [
@@ -75,9 +77,13 @@ WSGI_APPLICATION = 'CB_marketing.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'HOST': '127.0.0.1',
+        'NAME': 'cannybeewebsite',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+
     }
 }
 

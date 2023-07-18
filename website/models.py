7992639a27b3c8244d517859783fad4c8ead6888
piatekkrzysztof <krzysztof.piatek99@onet.pl>
2023-07-18 17:25:32 +0200

    Homepage,models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Blog_post(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL,)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Article(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL,)
+
+class Article_category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Question_category(models.Model):
+    name = models.CharField()
+
+class Question(models.Model):
+    category = models.ForeignKey(Question_category, on_delete=models.CASCADE)
+    contents = models.CharField(max_length=255)
+    ans_a = models.CharField()
+    ans_b = models.CharField()
+    ans_c = models.CharField()
+    ans_d = models.CharField()
+    correct = models.CharField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+class Result(models.Model):
+    category=models.ForeignKey(Question_category, on_delete=models.CASCADE)
+    amount=models.IntegerField()
+    user=models.ForeignKey(Profile,null=True,on_delete=models.SET_NULL)
+
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 def main(request):
     return render(request,'layout.html')
+
+def home(request):
+    return render(request,'home.html')
